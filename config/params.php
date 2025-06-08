66ad47af16815e4ef06ad91b75a0c2b3f151299e
RAdKuzur <drive16052003@gmail.com>
2025-06-08 13:15:52 +0400

    telegram token delete

--- config/params.php
+++ config/params.php
@@ -5,5 +5,4 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'requestToken' => '1234567890',
-    'telegramBotToken' => '7750029736:AAGG2af-WYQ7rIkjxMcz9jZwBmTGDa7xmOw',
 ];
